12b4d4465f3b49c8728035312254e02aea8238b4
dayoung <dayoungkim9105@gmail.com>
2022-04-11 19:23:36 +0900

    mongoDB로 전체 데이터 보내기

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, Input } from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
 
 const { Title: AntdTitle } = Typography;
 const { TextArea } = Input;
@@ -15,7 +16,7 @@ const Continents = [
   { key: 7, value: 'Antarctica' },
 ];
 
-const UploadProductPage = () => {
+const UploadProductPage = (props) => {
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
   const [price, setPrice] = useState('');
@@ -39,12 +40,39 @@ const UploadProductPage = () => {
     setImgs(newImages);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!title || !desc || !price || !continent | !imgs) {
+      return alert('모든 값이 필요합니다');
+    }
+
+    const body = {
+      //로그인된 사람의 ID
+      writer: props.user.userData._id,
+      title: title,
+      description: desc,
+      price: price,
+      images: imgs,
+      continent: continent,
+    };
+
+    Axios.post('/api/product', body).then((response) => {
+      if (response.data.success) {
+        alert('업로드에 성공했습니다.');
+        props.history.push('/');
+      } else {
+        alert('실패...');
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
         <AntdTitle level={2}>여행 상품 업로드</AntdTitle>
       </div>
-      <Form>
+      <Form onSubmitCapture={submitHandler}>
         <FileUpload refreshFunction={updateImages} />
 
         <label>이름</label>
@@ -60,7 +88,7 @@ const UploadProductPage = () => {
             </option>
           ))}
         </select>
-        <Button>확인</Button>
+        <Button htmlType='submit'>확인</Button>
       </Form>
     </div>
   );
