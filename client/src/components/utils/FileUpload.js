9852cf622228d46e7bf19b9beb657ceaad0632bb
dayoung <dayoungkim9105@gmail.com>
2022-04-11 15:19:50 +0900

    mongoDB name change + dropZone

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
+import { Icon } from 'antd';
 
 const FileUpload = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
       <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
         {({ getRootProps, getInputProps }) => (
           <section>
-            <div {...getRootProps()}>
+            <div
+              {...getRootProps()}
+              style={{
+                width: 300,
+                height: 240,
+                border: '1px solid lightgray',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
               <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
+              <Icon type='plus' style={{ fontSize: '3rem' }} />
             </div>
           </section>
         )}
