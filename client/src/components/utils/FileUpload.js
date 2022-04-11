d5faccc11ff8d36b1591539ff6ccd218928c25bb
dayoung <dayoungkim9105@gmail.com>
2022-04-11 14:36:38 +0900

    basic template

---
+++ client/src/components/utils/FileUpload.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Dropzone from 'react-dropzone';
+
+const FileUpload = () => {
+  return (
+    <div>
+      <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+    </div>
+  );
+};
+
+export default FileUpload;
